f4b70bd6a36f671de7bfd05ea08c6e468a5475b1
omer358 <omermaki358@gmail.com>
2023-09-24 12:56:48 +0200

    rest get list of student

--- src/main/java/com/example/springboottutorial/SpringBootTutorialApplication.java
+++ src/main/java/com/example/springboottutorial/SpringBootTutorialApplication.java
@@ -1,9 +1,14 @@
 package com.example.springboottutorial;
 
+import com.example.springboottutorial.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -14,8 +19,30 @@ public class SpringBootTutorialApplication {
     }
 
     @GetMapping("/")
-    public String hello() {
-        return "Hello world";
+    public List<Student> hello() {
+        return List.of(
+                new Student(
+                        1L,
+                        "Mariam",
+                        "mariam@exampl.com",
+                        LocalDate.of(1999, Month.APRIL, 5),
+                        24
+                ),
+                new Student(
+                        1L,
+                        "Huda",
+                        "huda@exampl.com",
+                        LocalDate.of(2000, Month.APRIL, 5),
+                        23
+                ),
+                new Student(
+                        1L,
+                        "Adel",
+                        "adel@exampl.com",
+                        LocalDate.of(1998, Month.APRIL, 5),
+                        25
+                )
+        );
     }
 
 }
